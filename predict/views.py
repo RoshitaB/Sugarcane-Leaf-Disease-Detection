24aadf83cb26e3b1b5f94c6f1b0be64a6f29c0c0
Roshita <roshita1608@gmail.com>
2022-07-12 21:37:24 +0530

    updated views

--- predict/views.py
+++ predict/views.py
@@ -3,53 +3,47 @@ from django.core.files.storage import FileSystemStorage
 import numpy as np
 import keras
 from keras.models import load_model
-from keras.preprocessing import image
-
-
-# Create your views here.
-
 
 model=load_model('models/Final_DenseNetSVM_Model.h5')
 
-
 def index(request):
     context={'a':1}
-    return render(request,'crop_index.html',context)
+    return render(request,'index.html',context)
 
 img_height, img_width=224,224
 
 def predictImage(request):
-    print (request)
-    print (request.POST.dict())
+    # print (request)
+    # print (request.POST.dict())
     fileObj=request.FILES['filePath']
     fs=FileSystemStorage()
     filePathName=fs.save(fileObj.name,fileObj)
     filePathName=fs.url(filePathName)
     testimage='.'+filePathName
+
     img = keras.utils.load_img(testimage, target_size=(img_height, img_width))
     x = keras.utils.img_to_array(img)
     x=x/255
     x=x.reshape(img_height, img_width,3)
     x = np.expand_dims(x, axis=0)
-    # with model_graph.as_default():
-    #     with tf_session.as_default():
+
     predi=model.predict(x)
-    print(predi)
-    classes_x=np.argmax(predi)
-    
-    print(classes_x)
+    # print(predi)
+
     classes=["Healthy","Red Rot", "Red Rust"]      
     MaxPosition=np.argmax(predi)  
+
     global prediction_label
     prediction_label=prediction_label=classes[MaxPosition]
     print(prediction_label)
+
     if(prediction_label=="Healthy"):
         pred_flag=True
     else:
         pred_flag=False
 
     context={'filePathName':filePathName,'predictedLabel':prediction_label,'pred_flag':pred_flag}
-    return render(request,'index.html',context) 
+    return render(request,'result.html',context) 
 
 
 
